1f8efdbc40ff94c5a00a646bab73f5d35680d5b5
Cynthia Huynh <115051720+synthwin@users.noreply.github.com>
2023-03-02 17:42:16 +0000

    Added carousel feature, created functions that populated DOM tree of homepage features

--- ecommerce-project/main.js
+++ ecommerce-project/main.js
@@ -11,7 +11,7 @@ function generateTopBarCategories(array) {
   }
 }
 
-// Generating Welcom Tile Content
+// Generating Welcome Tile Content
 function backgroundHomeImg(string) {
   var $backgroundImg = document.querySelector('#homepage-background-img');
   $backgroundImg.src = string;
@@ -47,151 +47,108 @@ function createShopNowButton(array) {
   array[0].appendChild($button);
 }
 
+// Query API for product images and populate images for carousel
+function carousel(productIdArray, interval) {
+  var productArray = new XMLHttpRequest();
+  productArray.open('GET', 'https://fakestoreapi.com/products');
+  productArray.responseType = 'json';
+  productArray.addEventListener('load', function (e) {
+    produceCarouselImg(e, productIdArray, interval);
+  });
+
+  function produceCarouselImg(e, productIdArray, interval) {
+    var $carousel = document.querySelector('.carousel-row');
+    var section = 0;
+
+    for (let i = 0; i < productIdArray.length; i++) {
+      if (i % interval === 0 && i === 0) {
+        var $ul = document.createElement('ul');
+        $ul.setAttribute('data-groupid', section);
+        $ul.className = 'carousel-group';
+        $carousel.appendChild($ul);
+        section++;
+      } else if (i % interval === 0 && i > 0) {
+        $ul = document.createElement('ul');
+        $ul.setAttribute('data-groupid', section);
+        $ul.className = 'carousel-group hidden';
+        $carousel.appendChild($ul);
+        section++;
+      }
+
+      var $spanImg = document.createElement('span');
+      var $carouselGroup = document.querySelector('.carousel-group');
+      var $imgProduct = document.createElement('img');
+      if (section === 1) {
+        $spanImg.className = 'carousel-tile';
+        $spanImg.setAttribute('data-spanId', i);
+        $imgProduct.setAttribute('data-imageid', i);
+        $imgProduct.src = productArray.response[productIdArray[i] - 1].image;
+        $imgProduct.className = 'carousel-image';
+        $spanImg.appendChild($imgProduct);
+        $carouselGroup.appendChild($spanImg);
+      } else {
+        var $carouselNodeList = document.querySelectorAll('.carousel-group');
+        $spanImg.className = 'carousel-tile';
+        $spanImg.setAttribute('data-spanId', i);
+        $imgProduct.setAttribute('data-imageid', i);
+        $imgProduct.src = productArray.response[productIdArray[i] - 1].image;
+        // $imgProduct.src = productArray[productIdArray[i] - 1].image;
+        $imgProduct.className = 'carousel-image';
+        $spanImg.appendChild($imgProduct);
+        $carouselNodeList[section - 1].appendChild($spanImg);
+      }
+    }
+
+    var $span = document.querySelectorAll('span');
+    $span.forEach(function (individualSpan) {
+      individualSpan.addEventListener('click', clickDirection);
+    });
+  }
+  productArray.send();
+}
+
+//  Change Carousel View by clicking first or last image
+function clickDirection(e) {
+  var $ulNodeList = document.querySelectorAll('ul');
+
+  if (e.target.tagName !== 'IMG') {
+    var spanId = parseInt(e.target.getAttribute('data-spanid'));
+  } else if (e.target.tagName !== 'SPAN') {
+    var imgId = parseInt(e.target.getAttribute('data-imageid'));
+  }
+
+  if (imgId % 4 === 0 || spanId % 4 === 0 || (imgId + 1) % 4 === 0 || (spanId + 1) % 4 === 0) {
+    var $hidden = document.querySelector('.hidden');
+    var groupId = parseInt($hidden.getAttribute('data-groupid'));
+    $hidden.classList.remove('hidden');
+
+    for (let i = 0; i < $ulNodeList.length; i++) {
+      if (groupId !== i) {
+        $ulNodeList[i].classList.add('hidden');
+      }
+    }
+  }
+  clearInterval(nIntervId);
+}
+
+// Automatic view change in carousel
+function cycle() {
+  var $ulNodeList = document.querySelectorAll('ul');
+  var $hidden = document.querySelector('.hidden');
+  var groupId = parseInt($hidden.getAttribute('data-groupid'));
+  $hidden.classList.remove('hidden');
+
+  for (let i = 0; i < $ulNodeList.length; i++) {
+    if (groupId !== i) {
+      $ulNodeList[i].classList.add('hidden');
+    }
+  }
+}
+
 var $welcomeTile = document.querySelector('.welcome');
 generateTopBarCategories(['Men', 'Women', 'Jewelry']);
 backgroundHomeImg('/images/woman-orange-edited.png');
 var populatedWelcomeDiv = backgroundText(['The Collection is Here', 'From dresses, shirts and jewerly']);
 createShopNowButton([populatedWelcomeDiv, 'SHOP NOW']);
-/* function getPokemonData(name) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://fakestoreapi.com/products/' + name);
-  xhr.responseType = 'json';
-  xhr.addEventListener('load', function () {
-    console.log(xhr.status);
-    console.log(xhr.response);
-  });
-  xhr.send();
-}
-
-getPokemonData('cubone'); */
-
-/* function addData(imgArray) {
-  var nextImgId = 1;
-  for (let i = 0; i < imgArray.length; i++) {
-    var imgValues = {};
-    imgValues.imgId = nextImgId;
-    imgValues.url = imgArray[i].url;
-    imgValues.name = imgArray[i].name;
-    nextImgId++;
-    data.entries.push(imgValues);
-  }
-  return data;
-}
-
-// Populating Circle Menu
-function populateCircles(data) {
-  for (let i = 1; i <= data.entries.length; i++) {
-    if (i !== 1) {
-      var newCircleSpan = createSpan(i);
-      $ul.appendChild(newCircleSpan);
-    } else {
-      newCircleSpan = createSpan(i);
-      var $solidId = newCircleSpan.querySelector('#solid-circle');
-      $solidId.classList.remove('hidden');
-      $ul.appendChild(newCircleSpan);
-    }
-  }
-}
-
-function createSpan(i) {
-  var $newSpan = document.createElement('span');
-  var $newIconOpen = document.createElement('i');
-  var $newIconSolid = document.createElement('i');
-
-  $newSpan.className = 'circle-icon';
-  $newIconOpen.setAttribute('data-num', i);
-  $newIconOpen.className = 'fa-regular fa-circle';
-  $newIconOpen.id = 'open-circle';
-  $newIconSolid.setAttribute('data-num', i);
-  $newIconSolid.className = 'fa-solid fa-circle hidden';
-  $newIconSolid.id = 'solid-circle';
-
-  $newSpan.appendChild($newIconOpen);
-  $newSpan.appendChild($newIconSolid);
-
-  return $newSpan;
-}
-
-// Circle Switch
-function switchCircle(e) {
-  e.preventDefault();
-  var i = Number(e.target.getAttribute('data-num'));
-  $img.src = data.entries[i - 1].url;
-  $img.alt = data.entries[i - 1].name;
-  for (let j = 0; j < $solid.length; j++) {
-    $solid[j].classList.add('hidden');
-  }
-  $solid[i - 1].classList.remove('hidden');
-}
-
-// Arrows Keys
-function arrow(e) {
-  var srcValue = $img.getAttribute('src');
-  for (let i = 0; i < data.entries.length; i++) {
-    if (srcValue === data.entries[i].url) {
-      var carouselIndex = i;
-    }
-  }
-  if (e.target.id === 'left') {
-    carouselIndex--;
-  } else {
-    carouselIndex++;
-  }
-
-  if (carouselIndex > data.entries.length - 1) {
-    carouselIndex = 0;
-    $img.src = data.entries[carouselIndex].url;
-    $img.alt = data.entries[carouselIndex].name;
-  } else if (carouselIndex < 0) {
-    carouselIndex = data.entries.length - 1;
-    $img.src = data.entries[carouselIndex].url;
-    $img.alt = data.entries[carouselIndex].name;
-  } else {
-    $img.src = data.entries[carouselIndex].url;
-    $img.alt = data.entries[carouselIndex].name;
-  }
-
-  for (let j = 0; j < $solid.length; j++) {
-    $solid[j].classList.add('hidden');
-  }
-  $solid[carouselIndex].classList.remove('hidden');
-}
-
-function countdown() {
-  var srcValue = $img.getAttribute('src');
-  for (let i = 0; i < data.entries.length; i++) {
-    if (srcValue === data.entries[i].url) {
-      var carouselIndex = i;
-    }
-  }
-  carouselIndex++;
-
-  if (carouselIndex > data.entries.length - 1) {
-    carouselIndex = 0;
-    $img.src = data.entries[carouselIndex].url;
-    $img.alt = data.entries[carouselIndex].name;
-  } else {
-    $img.src = data.entries[carouselIndex].url;
-    $img.alt = data.entries[carouselIndex].name;
-  }
-
-  for (let j = 0; j < $solid.length; j++) {
-    $solid[j].classList.add('hidden');
-  }
-  $solid[carouselIndex].classList.remove('hidden');
-}
-
-var $ul = document.querySelector('ul');
-var $img = document.querySelector('img');
-document.addEventListener('DOMContentLoaded', populateCircles(addData(pokemonImgArry)));
-var $span = document.querySelectorAll('span');
-var $solid = document.querySelectorAll('#solid-circle');
-var $left = document.querySelector('.fa-chevron-left');
-var $right = document.querySelector('.fa-chevron-right');
-
-$left.addEventListener('click', arrow);
-$right.addEventListener('click', arrow);
-
-$span.forEach(function (individualCircle) {
-  individualCircle.addEventListener('click', switchCircle);
-}); */
+carousel([1, 2, 3, 4, 10, 11, 12, 13], 4);
+var nIntervId = setInterval(cycle, 3000);
