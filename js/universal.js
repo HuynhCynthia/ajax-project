0d9140ad47e13d2f8c6c9a3f246fa51e894a33a1
Cynthia Huynh <115051720+synthwin@users.noreply.github.com>
2023-03-06 10:23:28 +0000

    Added links to mobile menu view

--- js/universal.js
+++ js/universal.js
@@ -1,6 +1,6 @@
 // Generating categories listed at the top bar
 function generateTopBarCategories(categoryMenuNames, categoriesApiNames) {
-  var $middleBar = document.querySelector('#middle-bar');
+  var $middleBar = document.querySelector('#middle-bar-desktop');
   for (let i = 0; i < categoryMenuNames.length; i++) {
     var $newAnchor = document.createElement('a');
     $newAnchor.className = 'categories-top-bar';
@@ -15,11 +15,43 @@ function populateStorage(e) {
   localStorage.setItem('categoryId', e.target.id);
 }
 
+function generateMobileCategories(categoryMenuNames, categoriesApiNames) {
+  var $middleBar = document.querySelector('#middle-bar-mobile');
+  for (let i = 0; i < categoryMenuNames.length; i++) {
+    var $newAnchor = document.createElement('a');
+    var $mobileLinks = document.createElement('div');
+    $mobileLinks.className = 'mobile-categories-span';
+    $newAnchor.className = 'categories-mobile-bar';
+    $newAnchor.href = '/html/category.html';
+    $newAnchor.id = categoriesApiNames[i];
+    $newAnchor.textContent = categoryMenuNames[i];
+    $mobileLinks.appendChild($newAnchor);
+    $middleBar.appendChild($mobileLinks);
+  }
+}
+
+// Show Mobile Menu
+function showMenu() {
+  var $mobileMenu = document.querySelector('.mobile-menu-container');
+  $mobileMenu.classList.remove('hidden');
+}
+
 // Generating categories listed at the top bar
 generateTopBarCategories(['Men', 'Women', 'Jewelry'], ['men\'s clothing', 'women\'s clothing', 'jewelery']);
+generateMobileCategories(['Men', 'Women', 'Jewelry'], ['men\'s clothing', 'women\'s clothing', 'jewelery']);
 
-// Category Page
+// Category Options
 var $categoryOptions = document.querySelectorAll('.categories-top-bar');
 $categoryOptions.forEach(function (individualCategoryLink) {
   individualCategoryLink.addEventListener('click', populateStorage);
 });
+
+// Mobile Category Options
+var $mobileCategoryOptions = document.querySelectorAll('.categories-mobile-bar');
+$mobileCategoryOptions.forEach(function (individualCategoryLink) {
+  individualCategoryLink.addEventListener('click', populateStorage);
+});
+
+// Show Menu
+var $dashMenu = document.querySelector('.right-icon');
+$dashMenu.addEventListener('click', showMenu);
